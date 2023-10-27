736620e23d6ccffb39810eecffddcd16e4dcdd82
The_Scientist777 <pmrojas195@gmail.com>
2023-10-27 12:08:13 -0500

    basic assignment

--- functions.h
+++ functions.h
@@ -1,1 +1,4 @@
- 
+ // 1.d Define function here.
+
+// 2.a Define function here.
+
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// 1.a Create global variables here.
+
+// 1.e Include header files here.
+
+int main() {
+  // 1.b Create make, model, year arrays here.
+
+  // 1.c Create local variables here.
+
+  // 1.f Welcome user here and call function.
+
+  // 2.b Call function here.
+
+  return 0;
+}
